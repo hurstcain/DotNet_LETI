5f219b89b2060ac4ef8d1c123f42aae0fb01adda
Ksenia Martynova <hurstcain@gmail.com>
2021-04-15 00:43:04 +0300

    Добавлены 3 задания

---
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class BinaryTree
+    {
+        // Корень дерева
+        public Node root
+        {
+            get;
+            set;
+        }
+
+        // Метод добавляет элемент в дерево
+        public void Add(int key)
+        {
+            Node parent = null;
+            Node current = this.root;
+            
+            while (current != null)
+            {
+                parent = current;
+
+                if (key < current.key)
+                {
+                    current = current.left;
+                }
+                else if (key > current.key)
+                {
+                    current = current.right;
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+
+            Node newNode = new Node();
+            newNode.key = key;
+
+            if (this.root == null)
+            {
+                this.root = newNode;
+            }
+            else
+            {
+                if (key < parent.key)
+                {
+                    parent.left = newNode;
+                }
+                else
+                {
+                    parent.right = newNode;
+                }
+            }
+
+            return;
+        }
+
+        // Метод удаляет элемент из дерева
+        private Node Remove(int key, Node parent)
+        {
+            if (parent == null) 
+            { 
+                return parent; 
+            }
+
+            // Если удаляемой значение меньше или больше ключа конкретной вершины,
+            // то спускаемся вниз по дереву
+            if (key < parent.key)
+            {
+                parent.left = Remove(key, parent.left);
+            }
+            else if (key > parent.key)
+            {
+                parent.right = Remove(key, parent.right);
+            }
+            else
+            {
+                // Если у найденной вершины нет или только один потомок
+                if (parent.left == null)
+                {
+                    return parent.right;
+                }
+                else if (parent.right == null)
+                {
+                    return parent.left;
+                }
+
+                parent.key = GetMin(parent.right);
+                parent.right = Remove(parent.key, parent.right);
+            }
+
+            return parent;
+        }
+
+        public void Remove(int key)
+        {
+            this.root = Remove(key, this.root);
+        }
+
+        // Метод нахождения вершины в дереве с указанный млючом key
+        private Node Find(int key, Node parent)
+        {
+            if (parent != null)
+            {
+                if (key == parent.key)
+                {
+                    return parent;
+                }
+                if (key < parent.key)
+                {
+                    return Find(key, parent.left);
+                }
+                else
+                {
+                    return Find(key, parent.right);
+                }
+            }
+            return null;
+        }
+
+        public Node Find(int key)
+        {
+            return this.Find(key, this.root);
+        }
+
+        private int GetMin(Node node)
+        {
+            int min = node.key;
+
+            while (node.left != null)
+            {
+                min = node.left.key;
+                node = node.left;
+            }
+
+            return min;
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BinaryTree
+{
+
+    class Program
+    {
+        static readonly int COUNT = 5;
+
+        static void PrintTree(Node root, int space_count = 0)
+        {
+            // Если корень пустой, то ничего не выводим
+            if (root == null)
+                return;
+
+            space_count += COUNT;
+
+            PrintTree(root.right, space_count);
+
+            Console.Write("\n");
+            for (int i = COUNT; i < space_count; i++)
+                Console.Write(" ");
+            Console.Write(root.key + "\n");
+
+            PrintTree(root.left, space_count);
+        }
+
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+ 
+            binaryTree.Add(10);
+            binaryTree.Add(2);
+            binaryTree.Add(22);
+            binaryTree.Add(4);
+            binaryTree.Add(12);
+            binaryTree.Add(11);
+            binaryTree.Add(6);
+            binaryTree.Add(13);
+            binaryTree.Add(5);
+            binaryTree.Add(1);
+
+            PrintTree(binaryTree.root);
+            Console.Write("-----------------------------------------------------------");
+
+            binaryTree.Remove(22);
+            PrintTree(binaryTree.root);
+            Console.Write("-----------------------------------------------------------");
+
+            PrintTree(binaryTree.Find(4));
+            Console.Write("-----------------------------------------------------------");
+        }
+    }
+}
